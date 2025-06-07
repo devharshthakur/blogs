c8a206fd7677379ee7549c0b5c0f59080bd92795
Harsh Thakur <178945966+devharshthakur@users.noreply.github.com>
2025-06-08 01:45:34 +0530

    chore(about): about page placeholder written
    - patched prettierignore config to ignore ui(shadcn ui ) for faster formatting

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,8 +3,11 @@ import React from 'react';
 const About = () => {
   return (
     <div>
-      <div className="flex min-h-screen items-center justify-center text-center text-4xl font-bold">
-        About Page
+      <div className="flex min-h-[calc(100vh-8rem)] flex-col items-center justify-center text-center">
+        <p className="text-8xl font-bold tracking-tight">About Page</p>
+        <p className="text-muted-foreground leading-tighter pt-3 text-xl leading-relaxed font-semibold">
+          This page contains information about who i am
+        </p>
       </div>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="flex min-h-[calc(100vh-8rem)] flex-col items-center justify-center text-center">
       <h1 className="mb-5 text-7xl font-bold tracking-tighter">Welcome to My Blog</h1>
-      <p className="text-muted-foreground max-w-500px mx-auto mb-15 max-w-3xl leading-relaxed tracking-tighter">
+      <p className="text-muted-foreground max-w-500px mx-auto mb-14 max-w-xl leading-relaxed tracking-tighter">
         A personal space where I share my thoughts, coding adventures, and insights on technology
         and design.
       </p>
