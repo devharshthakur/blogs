1605d39c983b13af885775140ebd5e4745fb2982
Harsh Thakur <178945966+devharshthakur@users.noreply.github.com>
2025-06-08 18:57:22 +0530

    feat(mdx): MDX working

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,21 @@
+import { Button } from '@/components/ui/button';
+import { Notebook } from 'lucide-react';
+import Link from 'next/link';
+
 export default function Home() {
   return (
     <div className="flex min-h-[calc(100vh-8rem)] flex-col items-center justify-center text-center">
-      <h1 className="mb-5 text-7xl font-bold tracking-tighter">Welcome to My Blog</h1>
-      <p className="text-muted-foreground max-w-500px mx-auto mb-14 max-w-xl leading-relaxed tracking-tighter">
+      <h1 className="mb-2 text-7xl font-bold tracking-tighter">Welcome to My Blog</h1>
+      <p className="text-muted-foreground mx-auto mb-7 max-w-140 leading-relaxed tracking-tighter">
         A personal space where I share my thoughts, coding adventures, and insights on technology
         and design.
       </p>
+      <Link href={'/all'} className="cursor-pointer hover:opacity-90">
+        <Button className="flex items-center">
+          <Notebook className="h-4 w-4" />
+          <span>View Blogs</span>
+        </Button>
+      </Link>
     </div>
   );
 }
